802679475d1027c192c61d5105625714b06eae59
Substicious <substicious@gmail.com>
2020-09-08 18:38:45 +0100

    .

--- script/docker-entrypoint.sh
+++ script/docker-entrypoint.sh
@@ -3,8 +3,9 @@
 if [ ! -f "$DATA_PATH/configs/server.properties" ]; then
   cp -R $DEFAULT_CONFIG_PATH/* $DATA_PATH/configs/
 
-if [ ! -d "$DATA_PATH/worlds" ]; then
-  mkdir -p $DATA_PATH/worlds
+  if [ ! -d "$DATA_PATH/worlds" ]; then
+    mkdir -p $DATA_PATH/worlds
+  fi
 fi
 
 ln -sb $DATA_PATH/configs/permissions.json $BDS/permissions.json
