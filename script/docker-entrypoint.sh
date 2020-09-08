9f508f63ebe0a8b30f4a72c93e79ed0f276017ba
Substicious <substicious@gmail.com>
2020-09-08 14:55:59 +0100

    AMENDED if statement line:3

--- script/docker-entrypoint.sh
+++ script/docker-entrypoint.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
-mkdir -p $DATA_PATH/configs
-
-# create file if not exist
-if [ ! -f "$DATA_PATH/configs/permissions.json" ]; then
+if [ ! -f "$BDS/server.properties" ]; then
   cp $DEFAULT_CONFIG_PATH/permissions.json $DATA_PATH/configs/permissions.json
-fi
-
-if [ ! -f "$DATA_PATH/configs/whitelist.json" ]; then
   cp $DEFAULT_CONFIG_PATH/whitelist.json $DATA_PATH/configs/whitelist.json
-fi
-
-if [ ! -f "$DATA_PATH/configs/server.properties" ]; then
   cp $DEFAULT_CONFIG_PATH/server.properties $DATA_PATH/configs/server.properties
 fi
 
