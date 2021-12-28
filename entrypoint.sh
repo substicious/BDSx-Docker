5b939e2fb0daecc95ef3ec1ea08616e2b42cf6b3
Substicious <68825499+substicious@users.noreply.github.com>
2021-12-28 12:58:27 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 # Make sure we're in the correct dir
 cd /home/container/
 
-STARTUP=`bdsx/bdsx.sh -y`
+STARTUP='bdsx/bdsx.sh -y'
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
