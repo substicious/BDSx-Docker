2b218e5da0cfd38615bf97e8ed876f2699bf98ca
Substicious <68825499+substicious@users.noreply.github.com>
2021-12-28 13:18:41 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,14 @@
 
 
 # Make sure we're in the correct dir
-cd /home/container/
-
-STARTUP='bdsx/bdsx.sh -y'
-
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container/$ ${MODIFIED_STARTUP}"
-
 cd /home/container/bdsx
-ls
-sleep 5
 
-cd /home/container/
+if [ ! -d ./bdsx ]
+then
+    git pull upstream master
+fi
 
 Xvfb :0 &
 export DISPLAY=:0
-
-# Run the Server
-${MODIFIED_STARTUP}
+npm i
+BDSX_YES=true BDSX_PERMANENT=true ./bdsx.sh
