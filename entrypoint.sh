808fe752c596468ebe1927414d81b0ec9a66e0c1
Substicious <68825499+substicious@users.noreply.github.com>
2021-12-28 21:08:16 +0000

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ then
     git config pull.ff only
     git pull upstream master
     git remote add upstream https://github.com/bdsx/bdsx.git
+else
+    Xvfb :0 &
+    export DISPLAY=:0
+
+    npm i
+    BDSX_YES=true BDSX_PERMANENT=true ./bdsx.sh
 fi
-
-Xvfb :0 &
-export DISPLAY=:0
-
-npm i
-BDSX_YES=true BDSX_PERMANENT=true ./bdsx.sh
