ca5f7c3a0b2808ff69d64ace5ae1bf7b48c5b923
Substicious <68825499+substicious@users.noreply.github.com>
2021-12-28 21:21:34 +0000

    :)

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ then
     git remote add upstream https://github.com/bdsx/bdsx.git
     git pull upstream master
 fi
-    #
+
 Xvfb :0 &
 export DISPLAY=:0
 
 npm i
-BDSX_YES=true BDSX_PERMANENT=true ./bdsx.sh
+BDSX_YES=true BDSX_PERMANENT=true /home/container/bdsx/bdsx.sh
