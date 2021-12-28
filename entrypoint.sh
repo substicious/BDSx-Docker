d6046d29444c54bd8959ee9b1f3ce24992ed20d3
Substicious <68825499+substicious@users.noreply.github.com>
2021-12-28 21:13:25 +0000

    Rearrange git statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ if [ ! -d ./bdsx ]
 then
     git init
     git config pull.ff only
-    git pull upstream master
     git remote add upstream https://github.com/bdsx/bdsx.git
-else
-    Xvfb :0 &
-    export DISPLAY=:0
-
-    npm i
-    BDSX_YES=true BDSX_PERMANENT=true ./bdsx.sh
+    git pull upstream master
 fi
+    #
+Xvfb :0 &
+export DISPLAY=:0
+
+npm i
+BDSX_YES=true BDSX_PERMANENT=true ./bdsx.sh
