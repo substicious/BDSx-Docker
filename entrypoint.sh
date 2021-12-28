af894c2ff13a58eb88c41095963f222ab37b8ada
Substicious <68825499+substicious@users.noreply.github.com>
2021-12-28 12:43:53 +0000

    Attempting to auto-true EULA

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 # Make sure we're in the correct dir
 cd /home/container/
 
-STARTUP=`bdsx/bdsx.sh`
+STARTUP=`bdsx/bdsx.sh -y`
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
@@ -12,7 +12,7 @@ echo ":/home/container/$ ${MODIFIED_STARTUP}"
 
 cd /home/container/bdsx
 ls
-sleep 15
+sleep 5
 
 cd /home/container/
 
@@ -20,4 +20,4 @@ Xvfb :0 &
 export DISPLAY=:0
 
 # Run the Server
-${MODIFIED_STARTUP} -y
+${MODIFIED_STARTUP}
