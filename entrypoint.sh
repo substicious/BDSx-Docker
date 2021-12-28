1ae4001167eac5a0e59a01f37a781f09ac558210
Substicious <68825499+substicious@users.noreply.github.com>
2021-12-28 12:32:20 +0000

    Try and Error #2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,5 @@
 #!/bin/ash
 
-Xvfb :0 &
-export DISPLAY=:0
-
-apk update
-apk add --no-cache --update
-git npm
-
-# make sure we're in correct dir
-if [ ! -d /mnt/server ]; then
-    mkdir /mnt/server
-fi
-
-cd /mnt/server
-
-# install
-if [ ! -d bdsx ]; then
-    git clone https://github.com/bdsx/bdsx.git
-fi
-
-# update
-cd bdsx
-git pull --ff-only
-
-./update.sh
 
 # Make sure we're in the correct dir
 cd /home/container/
@@ -39,6 +15,9 @@ ls
 sleep 15
 
 cd /home/container/
+
+Xvfb :0 &
+export DISPLAY=:0
 
 # Run the Server
 ${MODIFIED_STARTUP} BDSX_YES=true BDSX_PERMANENT=true 
